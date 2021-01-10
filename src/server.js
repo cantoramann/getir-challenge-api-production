f16545cf9462dd13e0c9d2af5521b8a86e97f4b3
Can Toraman <toraman@usc.edu>
2021-01-10 20:14:46 +0300

    deployment config

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const app = require('./app')
 require('./database')
 // require('dotenv').config()
 
+console.log(process.env.PORT)
 const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`App is successfully listening on port ${port}`)
