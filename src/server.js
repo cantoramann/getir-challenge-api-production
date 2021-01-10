9239b7a9f18193734cea2d072f80cb5a58d48157
Can Toraman <toraman@usc.edu>
2021-01-10 22:02:43 +0300

    Final deployment for production mode - env imports removed

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const app = require('./app')
 require('./database')
 // require('dotenv').config()
 
-console.log(process.env.PORT)
 const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`App is successfully listening on port ${port}`)
