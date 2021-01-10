6b3d9431fa403ffc0c74c18ca71985c37fda278d
Can Toraman <toraman@usc.edu>
2021-01-10 19:55:53 +0300

    deployment config

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const connect = async () => {
+  console.log(process.env.MONGO_URI)
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
